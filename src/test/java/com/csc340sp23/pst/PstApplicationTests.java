34561fd39ff21ef79e5419dbb2b570daa0e31ac4
John Abdellmelk <JNABDELL@uncg.edu>
2023-04-06 13:10:44 -0400

    springboot

---
+++ src/test/java/com/csc340sp23/pst/PstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340sp23.pst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
